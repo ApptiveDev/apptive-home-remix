fe098665956f0910e1d90f5be6c2557d46380204
cla6shade <cla6shade@gmail.com>
2025-02-23 12:50:35 +0900

    refactor: intersection observer을 이용하는 섹션을 클라이언트에서만 렌더링되도록 변경

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -5,13 +5,21 @@ import MainSection from '@/components/organisms/main/MainSection';
 import ActivitySection
   from '@/components/organisms/main/activity/ActivitySection';
 import GallerySection from '@/components/organisms/gallery/GallerySection';
+import { ClientOnly } from 'remix-utils/client-only';
 
 export default function Home() {
   return (
     <Page>
       <Qna />
       <HeroSection id="hero-section" />
-      <MainSection id="main-section" mode="dark" />
+      <ClientOnly>
+        {/* TODO: fallback UI 추가 */}
+        {
+          () => (
+            <MainSection id="main-section" mode="dark" />
+          )
+        }
+      </ClientOnly>
       <ActivitySection id="activity-section" />
       <GallerySection id="gallery-section" />
     </Page>
