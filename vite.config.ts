446949b1f7f8e02cb35ee6873a22caba0486a6a2
cla6shade <cla6shade@gmail.com>
2025-03-02 13:19:34 +0900

    chore: add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
+/// <reference types="vitest/config" />
+
 import { reactRouter } from "@react-router/dev/vite";
-import { defineConfig } from "vite";
+import { defineConfig, type UserConfigExport } from 'vite';
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [reactRouter(), tsconfigPaths()],
-});
+  test: {
+    browser: {
+      enabled: true,
+      provider: 'playwright',
+      instances: [
+        { browser: 'chromium' },
+      ],
+    },
+  },
+} as UserConfigExport);
